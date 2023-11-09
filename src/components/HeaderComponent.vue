0dcff4cbe14a00df496b9373373614a3f17c734a
Trotteriniku <stefanozidda3@gmail.com>
2023-11-09 18:45:37 +0100

    main 1

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,22 +1,42 @@
 <template>
-    <header class="">
+    <header >
+        <div class=" d-flex justify-content-lg-evenly py-4  ">
+            <img src="../assets/images/dc-logo.png" alt="dc logo">
 
-        <img class="" src="../assets/images/dc-logo.png" alt="dc logo">
-
-        <ul class="nav">
+        <ul class="nav align-content-center justify-content-end ms-5">
             <li class="nav-item">
-                <a class="nav-link"  href="#">Active</a>
+                <a class="nav-link text-secondary"  href="#">CHARACTERS</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link text-secondary" href="#">COMICS</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link text-secondary" href="#">MOVIES</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Disabled</a>
+                <a class="nav-link text-secondary" href="#">TV</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">GAMES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">COLLECTIBLES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">VIDEOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">FANS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">NEWS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary" href="#">SHOP</a>
             </li>
         </ul>
+        </div>
+        
     </header>
 </template>
 
@@ -38,5 +58,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+    header{
+        li{
+        font-weight: 700;
+        }
+        img{
+          max-width: 90px;
+        }
+    }
+    
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
-        main
+        <div class="container d-flex align-content-center ">
+            <p class=" text-light fs-3 fw-bold "> => Content goes Here &lt= </p>
+        </div>
     </main>
 </template>
 
@@ -22,5 +24,12 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/variabiles' as *;
+main{
+    background-color: $black--bg;
 
+    div{
+        height: 138px;
+    }
+}
 </style>
