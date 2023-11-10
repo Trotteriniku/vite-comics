f92ae23c90cf8739aca573db5469f2f733de3d30
Trotteriniku <stefanozidda3@gmail.com>
2023-11-10 01:42:16 +0100

    navhover

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,38 +3,42 @@
         <div class=" d-flex justify-content-lg-evenly py-4  ">
             <img src="../assets/images/dc-logo.png" alt="dc logo">
 
-        <ul class="nav align-content-center justify-content-end ms-5">
-            <li class="nav-item">
-                <a class="nav-link text-secondary"  href="#">CHARACTERS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">COMICS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">MOVIES</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">TV</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">GAMES</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">COLLECTIBLES</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">VIDEOS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">FANS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">NEWS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-secondary" href="#">SHOP</a>
-            </li>
-        </ul>
+            <ul class="nav align-content-center justify-content-end ms-5">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary"  href="#">CHARACTERS</a>
+                </li>
+                <li class="nav-item">
+
+                    <a class="nav-link text-secondary abar" href="#">COMICS
+                        <div class=" bg-primary text-end d-none "></div>
+                    </a>
+                    
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">MOVIES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">TV</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">GAMES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">COLLECTIBLES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">FANS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">NEWS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">SHOP</a>
+                </li>
+            </ul>
         </div>
         
     </header>
@@ -58,10 +62,22 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/variabiles' as *;
     header{
         li{
         font-weight: 700;
         }
+
+        .abar{
+           &:hover{
+            color: red;
+            // div{
+            // width: 100%;
+            // height: 7px;
+            // display: block;
+            // }
+           } 
+        }   
         img{
           max-width: 90px;
         }
